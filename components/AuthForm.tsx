22f8863a95e6e0ec47662226c2f3d5d9a9b9f8d4
Phanindra <phanindragadireddy2@gmail.com>
2025-02-21 15:18:07 +0530

    backend started for auth

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -1,5 +1,5 @@
 "use client"
-
+import Image from "next/image"
 import { z } from "zod"
 import { zodResolver } from "@hookform/resolvers/zod"
 import { useForm } from "react-hook-form"
@@ -16,20 +16,29 @@ import {
     FormMessage,
 } from "@/components/ui/form"
 import { Input } from "@/components/ui/input"
-
-const formSchema = z.object({
-    username: z.string().min(2).max(50),
-})
-
+import { useState } from "react"
+import  Link  from "next/link"
 
 type FormType = 'sign-in' | 'sign-up'
 
 const AuthForm = ({ type }: { type: FormType }) => {
 
+    const authFormSchema = (formType :FormType) => {
+        return z.object({
+            email:z.string().email(),
+            fullName:formType === "sign-up" ? z.string().min(2).max(50) :z.string().optional(), 
+        })
+    }
+
+    const [isLoading, setIsLoading] = useState(false)
+    const [errorMessage, setErrorMessage] = useState('')
+
+    const formSchema = authFormSchema(type)
     const form = useForm<z.infer<typeof formSchema>>({
         resolver: zodResolver(formSchema),
         defaultValues: {
-            username: "",
+            fullName: "",
+            email:""
         },
     })
 
@@ -37,6 +46,9 @@ const AuthForm = ({ type }: { type: FormType }) => {
     const onSubmit = async (values: z.infer<typeof formSchema>) => {
         console.log(values)
     }
+
+  
+
     return <>
         <Form {...form}>
             <form onSubmit={form.handleSubmit(onSubmit)} className="auth-form">
@@ -73,9 +85,25 @@ const AuthForm = ({ type }: { type: FormType }) => {
                         </FormItem>
                     )}
                 />
-                <Button type="submit" className="form-submit-button">
+                <Button type="submit" className="form-submit-button" disabled={isLoading}>
                     {type === "sign-in" ? "Sign In" : "Sign Up"}
+
+                    {isLoading && ( 
+                        <Image src = '/assets/icons/loader.svg' width={24} height={24} className="ml-2 animate-spin" alt = 'logo' />
+                        )}
                 </Button>
+                {errorMessage && (
+                    <p className="error-message">*{errorMessage}</p>
+                )}
+
+                <div className="body-2 flex justify-center">
+                    <p className="text-light-100">
+                        {type === "sign-in" ? "Don't have an account" : "Already have an account"}
+                    </p>
+                    <Link className="ml-1 font-medium text-brand" href = {type === "sign-in"? "/sign-up" : "/sign-in"}>
+                        {type === "sign-in" ? "Sign Up" : "Sign In"}
+                    </Link>
+                </div>
             </form>
         </Form>
         {/* OTP verification */}
